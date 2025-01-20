0de850e05d08b0e2c3bbff03586bc4c964011cbc
Gustavo Ferreira <gustavo.ghferreira1@gmail.com>
2025-01-20 17:10:21 -0300

    Checks OK

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,9 +34,9 @@
             radioButtonInputFahrenheit = new RadioButton();
             radioButtonInputCelsius = new RadioButton();
             groupBox2 = new GroupBox();
-            radioButton1 = new RadioButton();
-            radioButton2 = new RadioButton();
-            radioButton3 = new RadioButton();
+            radioButtonOutKelvin = new RadioButton();
+            radioButtonOutFahrenheit = new RadioButton();
+            radioButtonOutCelsius = new RadioButton();
             textBoxInput = new TextBox();
             textBoxOutput = new TextBox();
             buttonConvert = new Button();
@@ -68,6 +68,7 @@
             radioButtonInputKelvin.TabIndex = 2;
             radioButtonInputKelvin.Text = "Kelvin";
             radioButtonInputKelvin.UseVisualStyleBackColor = true;
+            radioButtonInputKelvin.CheckedChanged += radioButtonInputKelvin_CheckedChanged;
             // 
             // radioButtonInputFahrenheit
             // 
@@ -79,6 +80,7 @@
             radioButtonInputFahrenheit.TabIndex = 1;
             radioButtonInputFahrenheit.Text = "Fahrenheit";
             radioButtonInputFahrenheit.UseVisualStyleBackColor = true;
+            radioButtonInputFahrenheit.CheckedChanged += radioButtonInputFahrenheit_CheckedChanged;
             // 
             // radioButtonInputCelsius
             // 
@@ -90,12 +92,13 @@
             radioButtonInputCelsius.TabIndex = 0;
             radioButtonInputCelsius.Text = "Celsius";
             radioButtonInputCelsius.UseVisualStyleBackColor = true;
+            radioButtonInputCelsius.CheckedChanged += radioButtonInputCelsius_CheckedChanged;
             // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(radioButton1);
-            groupBox2.Controls.Add(radioButton2);
-            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Controls.Add(radioButtonOutKelvin);
+            groupBox2.Controls.Add(radioButtonOutFahrenheit);
+            groupBox2.Controls.Add(radioButtonOutCelsius);
             groupBox2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             groupBox2.Location = new Point(192, 58);
             groupBox2.Name = "groupBox2";
@@ -104,38 +107,38 @@
             groupBox2.TabStop = false;
             groupBox2.Text = "Saída";
             // 
-            // radioButton1
+            // radioButtonOutKelvin
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Font = new Font("Segoe UI", 9F);
-            radioButton1.Location = new Point(3, 69);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(57, 19);
-            radioButton1.TabIndex = 2;
-            radioButton1.Text = "Kelvin";
-            radioButton1.UseVisualStyleBackColor = true;
+            radioButtonOutKelvin.AutoSize = true;
+            radioButtonOutKelvin.Font = new Font("Segoe UI", 9F);
+            radioButtonOutKelvin.Location = new Point(3, 69);
+            radioButtonOutKelvin.Name = "radioButtonOutKelvin";
+            radioButtonOutKelvin.Size = new Size(57, 19);
+            radioButtonOutKelvin.TabIndex = 2;
+            radioButtonOutKelvin.Text = "Kelvin";
+            radioButtonOutKelvin.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // radioButtonOutFahrenheit
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Font = new Font("Segoe UI", 9F);
-            radioButton2.Location = new Point(3, 44);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(81, 19);
-            radioButton2.TabIndex = 1;
-            radioButton2.Text = "Fahrenheit";
-            radioButton2.UseVisualStyleBackColor = true;
+            radioButtonOutFahrenheit.AutoSize = true;
+            radioButtonOutFahrenheit.Font = new Font("Segoe UI", 9F);
+            radioButtonOutFahrenheit.Location = new Point(3, 44);
+            radioButtonOutFahrenheit.Name = "radioButtonOutFahrenheit";
+            radioButtonOutFahrenheit.Size = new Size(81, 19);
+            radioButtonOutFahrenheit.TabIndex = 1;
+            radioButtonOutFahrenheit.Text = "Fahrenheit";
+            radioButtonOutFahrenheit.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // radioButtonOutCelsius
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            radioButton3.Location = new Point(3, 19);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(62, 19);
-            radioButton3.TabIndex = 0;
-            radioButton3.Text = "Celsius";
-            radioButton3.UseVisualStyleBackColor = true;
+            radioButtonOutCelsius.AutoSize = true;
+            radioButtonOutCelsius.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButtonOutCelsius.Location = new Point(3, 19);
+            radioButtonOutCelsius.Name = "radioButtonOutCelsius";
+            radioButtonOutCelsius.Size = new Size(62, 19);
+            radioButtonOutCelsius.TabIndex = 0;
+            radioButtonOutCelsius.Text = "Celsius";
+            radioButtonOutCelsius.UseVisualStyleBackColor = true;
             // 
             // textBoxInput
             // 
@@ -161,6 +164,7 @@
             buttonConvert.TabIndex = 4;
             buttonConvert.Text = "Converter";
             buttonConvert.UseVisualStyleBackColor = true;
+            buttonConvert.Click += buttonConvert_Click;
             // 
             // buttonClean
             // 
@@ -171,6 +175,7 @@
             buttonClean.TabIndex = 5;
             buttonClean.Text = "Limpar";
             buttonClean.UseVisualStyleBackColor = true;
+            buttonClean.Click += buttonClean_Click;
             // 
             // Form1
             // 
@@ -204,9 +209,9 @@
         private RadioButton radioButtonInputFahrenheit;
         private RadioButton radioButtonInputCelsius;
         private GroupBox groupBox2;
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
-        private RadioButton radioButton3;
+        private RadioButton radioButtonOutKelvin;
+        private RadioButton radioButtonOutFahrenheit;
+        private RadioButton radioButtonOutCelsius;
         private TextBox textBoxInput;
         private TextBox textBoxOutput;
         private Button buttonConvert;
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,55 @@ namespace Conversor_de_temperaturas
         {
             InitializeComponent();
         }
+
+        private void radioButtonInputCelsius_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonOutCelsius.Checked = false;
+
+            radioButtonOutCelsius.Enabled = false;
+            radioButtonOutFahrenheit.Enabled = true;
+            radioButtonOutKelvin.Enabled = true;
+        }
+
+        private void radioButtonInputFahrenheit_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonOutFahrenheit.Checked = false;
+
+            radioButtonOutCelsius.Enabled = true;
+            radioButtonOutFahrenheit.Enabled = false;
+            radioButtonOutKelvin.Enabled = true;
+        }
+
+        private void radioButtonInputKelvin_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonOutKelvin.Checked = false;
+
+            radioButtonOutCelsius.Enabled = true;
+            radioButtonOutFahrenheit.Enabled = true;
+            radioButtonOutKelvin.Enabled = false;
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e)
+        {
+            radioButtonInputCelsius.Checked = false;
+            radioButtonInputFahrenheit.Checked = false;
+            radioButtonInputKelvin.Checked = false;
+
+            radioButtonOutCelsius.Checked = false;
+            radioButtonOutFahrenheit.Checked = false;
+            radioButtonOutKelvin.Checked = false;
+
+            radioButtonOutCelsius.Enabled = true;
+            radioButtonOutFahrenheit.Enabled = true;
+            radioButtonOutKelvin.Enabled = true;
+
+            textBoxInput.Text = "";
+            textBoxOutput.Text = "";
+        }
     }
 }
